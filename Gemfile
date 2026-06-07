a5aaababd76cb43d63f9080a213b4cb04ac38aa4
Dewayne VanHoozer <dvanhoozer@gmail.com>
2026-06-06 20:38:25 -0500

    added simplecov

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.16"
 gem "minitest-reporters"
+gem "simplecov", require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/vendor/'
+
+  add_group 'Discovery', 'lib/robot_lab/discovery'
+
+  enable_coverage :branch
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "robot_lab/discovery"
